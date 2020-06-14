ea83d7a41e7c7d6f96164522f5dabc4bbcc0a331
Yury Zubchenko <yury.zubchenko@gmail.com>
2020-06-14 19:52:36 +0000

    demo-build

---
+++ build/precache-manifest.cc6dbf382bd59cb10979b23e88b67913.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb645c137a1453158e91ae28bc6a431b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1715bbd697e3b351e933",
+    "url": "/static/css/main.f154c4f4.chunk.css"
+  },
+  {
+    "revision": "385b4901e21b4a1f1515",
+    "url": "/static/js/2.7b78d68e.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.7b78d68e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1715bbd697e3b351e933",
+    "url": "/static/js/main.c10cd722.chunk.js"
+  },
+  {
+    "revision": "90f866211726ec9a6fee",
+    "url": "/static/js/runtime-main.40cdc986.js"
+  }
+]);
